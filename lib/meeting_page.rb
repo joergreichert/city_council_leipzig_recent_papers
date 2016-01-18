1df5dad6cdea480d98e7085e7c513f85718ce6c5
Andreas Haller <andreashaller@gmail.com>
2016-01-18 21:35:14 +0100

    Add initial MeetingPage class

---
+++ lib/meeting_page.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+require 'page'
+require 'pry'
+
+class MeetingPage < Page
+
+  def meeting
+    time = Time.parse(doc.css("table.tk1 table.tk1 td:contains('Datum') ~ td").first.text)
+    start_value, end_value = doc.css("table.tk1 table.tk1 td:contains('Zeit') ~ td").first.text.split(" - ")
+    {
+      id: uri,
+      type: 'oparl:Meeting',
+      name: doc.css("table.tk1 table.tk1 td:contains('Bezeichnung') ~ td").first.text,
+      start: Time.parse(start_value, time).iso8601,
+      end: Time.parse(end_value, time).iso8601,
+    }
+  end
+end
---
+++ test/meeting_page_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'meeting_page'
+require 'fixture_fetcher'
+
+class TestMeetingPage < Minitest::Test
+  def test_meeting
+    uri = 'https://ratsinfo.leipzig.de/bi/to010.asp?SILFDNR=1002144'
+    meeting = MeetingPage.new(uri, fetcher: FixtureFetcher).meeting
+    expected_meeting = {
+      id: 'https://ratsinfo.leipzig.de/bi/to010.asp?SILFDNR=1002144',
+      type: 'oparl:Meeting',
+      name: 'Ratsversammlung',
+      # room: 'Neues Rathaus, Sitzungssaal des Stadtrates',
+      # street_address: 'Martin-Luther-Ring 4-6',
+      # postal_code: '04109',
+      # locality: 'Neues Rathaus',
+      start: '2015-11-11T18:00:00+01:00',
+      end: '2015-11-11T19:30:00+01:00'
+    }
+    assert_equal(expected_meeting, meeting)
+  end
+end
